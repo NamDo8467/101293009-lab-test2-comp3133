88829b27a61b4a74b9c004f308e809720da6d435
NamDo8467 <huynhnamdo@gmail.com>
2023-04-03 17:39:27 -0400

    change styles

--- src/app/components/missiondetail/missiondetail.component.ts
+++ src/app/components/missiondetail/missiondetail.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Mission } from 'src/app/Mission';
 @Component({
   selector: 'app-missiondetail',
   templateUrl: './missiondetail.component.html',
-  styleUrls: ['./missiondetail.component.css']
+  styleUrls: ['./missiondetail.component.css'],
 })
 export class MissiondetailComponent {
+  @Input() mission: any;
+  @Input() toggleOpenMissionDetail: any;
 
+  toggleMissionDetail() {
+    // alert("Yes")
+    this.toggleOpenMissionDetail();
+  }
 }
